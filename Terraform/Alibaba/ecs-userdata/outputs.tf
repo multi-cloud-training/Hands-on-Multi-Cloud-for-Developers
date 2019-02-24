e1b3f633c12a51289e04b01306af37223ea686e6
multicloud-training <45056457+multicloud-training@users.noreply.github.com>
2019-02-24 16:30:48 -0500

    Add files via upload

---
+++ Terraform/Alibaba/ecs-userdata/outputs.tf
@@ -0,0 +1,7 @@
+output "ecs_id" {
+  value = "${alicloud_instance.website.id}"
+}
+
+output "ecs_public_ip" {
+  value = "${alicloud_instance.website.public_ip}"
+}
---
+++ Terraform/Alibaba/slb/variables.tf
@@ -0,0 +1,11 @@
+variable "slb_name" {
+  default = "slb_worder"
+}
+
+variable "internet_charge_type" {
+  default = "PayByTraffic"
+}
+
+variable "internet" {
+  default = true
+}
