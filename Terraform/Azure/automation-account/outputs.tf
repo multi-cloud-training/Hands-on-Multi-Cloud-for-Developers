cfa2558941645237c7361382413362639fa41d2c
multicloud-training <45056457+multicloud-training@users.noreply.github.com>
2019-02-24 16:19:47 -0500

    Add files via upload

---
+++ Terraform/Azure/automation-account/outputs.tf
@@ -0,0 +1,7 @@
+output "automation_schedule-start_time" {
+  value = "${azurerm_automation_schedule.one-time.start_time}"
+}
+
+output "automation_schedule-week-interval" {
+  value = "${azurerm_automation_schedule.hour.interval}"
+}
